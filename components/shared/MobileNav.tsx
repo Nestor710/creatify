198893bf45c888587e6a968278b39b0173a39b6d
Nestor710 <nestorlima710@gmail.com>
2024-06-07 20:47:05 -0500

    logo changed

--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -8,7 +8,6 @@ import {
 import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs"
 import Image from "next/image"
 import Link from "next/link"
-import NavList from "./NavList"
 import { Button } from "../ui/button"
 import { navLinks } from "@/constants"
 import { usePathname } from "next/navigation"
@@ -21,7 +20,7 @@ const MobileNav = () => {
         <header className="header">
             <Link href="/" className="flex items-center gap-2 md:py-2">
                 <Image 
-                    src="/assets/images/logo-text.svg"
+                    src="/logo.png"
                     alt="logo"
                     width={180}
                     height={28}
@@ -43,7 +42,7 @@ const MobileNav = () => {
                         <SheetContent className="sheet-content sm:w-64">
                             <>
                                 <Image 
-                                    src="/assets/images/logo-text.svg"
+                                    src="/logo.png"
                                     alt="logo"
                                     width={152}
                                     height={23}
