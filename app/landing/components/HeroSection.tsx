88a142c2935dc49de5f08c144772a4119eeb852a
Nestor710 <nestorlima710@gmail.com>
2025-05-16 16:13:50 -0500

    hero section new styles and image hero

--- app/landing/components/HeroSection.tsx
+++ app/landing/components/HeroSection.tsx
@@ -1,46 +1,42 @@
-// src/components/HeroSection.tsx
+
 "use client";
 
 import Image from "next/image";
-import { ChevronRight } from "lucide-react";
+import Link from "next/link";
+import { useAuth } from "@clerk/nextjs";
+import GetStartedBtn from "@/components/ui/GetStartedBtn";
 
 export function HeroSection() {
+	const { userId } = useAuth();
+
 	return (
 		<section className="pt-32 pb-20 bg-gradient-to-b from-orange-50 to-cream-100">
 			<div className="container mx-auto px-4">
 				<div className="max-w-3xl mx-auto text-center">
-					<h1 className="text-4xl md:text-5xl font-bold mb-6 text-gray-900">
+					<h1 
+					className="text-4xl md:text-5xl font-bold mb-6 gradient-orange-text">
 						Image and Video Editing with AI in Seconds
 					</h1>
-					<p className="text-xl text-gray-700 mb-10 max-w-2xl mx-auto">
+					<p className="text-xl text-gray-700/80 mb-10 max-w-2xl mx-auto">
 						Remove backgrounds, edit images and videos quickly with our
 						artificial intelligence technology.
 					</p>
 					<div className="flex flex-col sm:flex-row gap-4 justify-center">
-						<button
-							onClick={() => console.log("Start now")}
-							className="px-6 py-3 rounded-md bg-orange-500 text-white font-medium hover:bg-orange-600 transition-all flex items-center justify-center"
-						>
-							Get Started <ChevronRight className="ml-2" size={16} />
-						</button>
-						<button
-							onClick={() => console.log("View demos")}
-							className="px-6 py-3 rounded-md border border-orange-500 text-orange-500 font-medium hover:bg-orange-100 transition-colors"
-						>
-							View Demos
-						</button>
+						<GetStartedBtn />
 					</div>
 				</div>
 
-				<div className="mt-16 max-w-5xl mx-auto rounded-lg overflow-hidden ">
-					<div className="aspect-[16/9] relative">
-						<Image
-							src="/hero.webp"
-							alt="Platform demo"	
-							fill
-							className="object-cover"
-							priority
-						/>
+				<div className="max-w-5xl mx-auto rounded-lg overflow-hidden ">
+					<div id="view-demo" className="aspect-[16/9] relative hover:!transform-none transition-all duration-500" style={{transform: "translate(0px, 0px) perspective(8116.92px) scale(1) skew(0deg, 0deg) rotateX(30deg) rotateY(0deg) rotateZ(0deg)"}}>
+						<Link href={userId ? "/dashboard" : "/sign-in"} aria-label="Platform Dashboard" role="button">
+							<Image
+								src="/hero.webp"
+								alt="Platform demo"
+								fill
+								className="object-cover"
+								priority
+							/>
+						</Link>
 					</div>
 				</div>
 			</div>
