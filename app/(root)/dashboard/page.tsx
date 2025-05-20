c1533461ef11a841832e338b515ee9c4a691cf65
Leonardo Araujo <leonardoaraujo.oct@gmail.com>
2025-05-20 10:04:58 -0500

    Fixed crop svg icon in dashboard size

--- app/(root)/dashboard/page.tsx
+++ app/(root)/dashboard/page.tsx
@@ -1,6 +1,6 @@
 import { Collection } from "@/components/shared/Collection"
 import { navLinks } from "@/constants"
-import { getAllImages, getUserImages } from "@/lib/actions/image.actions"
+import { getUserImages } from "@/lib/actions/image.actions"
 import Image from "next/image"
 import Link from "next/link"
 import { auth } from "@clerk/nextjs/server"
@@ -8,46 +8,46 @@ import { redirect } from "next/navigation"
 import { getUserById } from "@/lib/actions/user.actions"
 
 const Home = async ({ searchParams }: SearchParamProps) => {
-  const page = Number(searchParams?.page) || 1;
-  const { userId } = auth();
+	const page = Number(searchParams?.page) || 1;
+	const { userId } = auth();
 
-  if (!userId) redirect("/sign-in");
+	if (!userId) redirect("/sign-in");
 
-  const user = await getUserById(userId);
+	const user = await getUserById(userId);
   const images = await getUserImages({ page, userId: user._id })
 
-  return (
-    <>
-      <section className="home">
-        <h1 className="home-heading">Bring Your Ideas to Life with Creatify</h1>
-        <ul className="flex-center w-full gap-20">
-          {navLinks.slice(1, 7).map((link) => (
-            <Link 
-              key={link.route}
-              href={link.route}
-              className="flex-center flex-col gap-2"
-            >
-              <li className="flex-center w-fit rounded-full bg-white p-4 shadow-md shadow-white hover:shadow-white hover:shadow-lg hover:scale-110 transition-all">
-                <Image
-                  src={link.icon}
-                  alt="Image"
-                  width={24}
-                  height={24}
-                />
-              </li>
-              <p className="p-14-medium text-center text-white">{link.label}</p>
-            </Link>
-          ))}
-        </ul>
-      </section>
-      <section className="sm:mt-12">
-        <Collection
-          images={images?.data}
-          totalPages={images?.totalPages}
-          page={page}
-        />
-      </section>
-    </>
+	return (
+		<>
+			<section className="home">
+				<h1 className="home-heading">Bring Your Ideas to Life with Creatify</h1>
+				<ul className="flex-center w-full gap-20">
+					{navLinks.slice(1, 7).map((link) => (
+						<Link
+							key={link.route}
+							href={link.route}
+							className="flex-center flex-col gap-2"
+						>
+							<li className="flex-center w-fit rounded-full bg-white p-4 shadow-md shadow-white hover:shadow-white hover:shadow-lg hover:scale-110 transition-all">
+								<Image
+									src={link.icon}
+									alt="Image"
+									width={link.iconWidth || 24}
+									height={link.iconHeight || 24}
+								/>
+							</li>
+							<p className="p-14-medium text-center text-white">{link.label}</p>
+						</Link>
+					))}
+				</ul>
+			</section>
+			<section className="sm:mt-12">
+				<Collection
+					images={images?.data}
+					totalPages={images?.totalPages}
+					page={page}
+				/>
+			</section>
+		</>
   )
 }
 
--- constants/index.ts
+++ constants/index.ts
@@ -1,266 +1,268 @@
 export const navLinks = [
-    {
-      label: "Home",
-      route: "/",
-      icon: "/assets/icons/home.svg",
-    },
-    {
-      label: "Image Restore",
-      route: "/transformations/add/restore",
-      icon: "/assets/icons/image.svg",
-    },
-    {
-      label: "Generative Fill",
-      route: "/transformations/add/fill",
-      icon: "/assets/icons/stars.svg",
-    },
-    {
-      label: "Object Remove",
-      route: "/transformations/add/remove",
-      icon: "/assets/icons/scan.svg",
-    },
-    {
-      label: "Object Recolor",
-      route: "/transformations/add/recolor",
-      icon: "/assets/icons/filter.svg",
-    },
-    {
-      label: "Background Remove",
-      route: "/transformations/add/removeBackground",
-      icon: "/assets/icons/camera.svg",
-    },
-    {
-      label: "Clip Crop",
-      route: "/video/crop",
-      icon: "/assets/icons/crop.svg",
-    },
-    {
-      label: "Profile",
-      route: "/profile",
-      icon: "/assets/icons/profile.svg",
-    },
-    {
-      label: "Buy Credits",
-      route: "/credits",
-      icon: "/assets/icons/bag.svg",
-    },
-  ];
+	{
+		label: "Home",
+		route: "/",
+		icon: "/assets/icons/home.svg",
+	},
+	{
+		label: "Image Restore",
+		route: "/transformations/add/restore",
+		icon: "/assets/icons/image.svg",
+	},
+	{
+		label: "Generative Fill",
+		route: "/transformations/add/fill",
+		icon: "/assets/icons/stars.svg",
+	},
+	{
+		label: "Object Remove",
+		route: "/transformations/add/remove",
+		icon: "/assets/icons/scan.svg",
+	},
+	{
+		label: "Object Recolor",
+		route: "/transformations/add/recolor",
+		icon: "/assets/icons/filter.svg",
+	},
+	{
+		label: "Background Remove",
+		route: "/transformations/add/removeBackground",
+		icon: "/assets/icons/camera.svg",
+	},
+	{
+		label: "Clip Crop",
+		route: "/video/crop",
+		icon: "/assets/icons/crop.svg",
+		iconWidth: 35,
+		iconHeight: 35,
+	},
+	{
+		label: "Profile",
+		route: "/profile",
+		icon: "/assets/icons/profile.svg",
+	},
+	{
+		label: "Buy Credits",
+		route: "/credits",
+		icon: "/assets/icons/bag.svg",
+	},
+];
 
-  export const navLinksNew = [
-    {
-      label: "Home",
-      route: "/dashboard",
-      icon: "/assets/icons/home.svg",
-    },
-    {
-      label: "Images",
-      icon: "/assets/icons/image-list.svg",
-      subLinks: [
-        {
-          label: "Image Restore",
-          route: "/transformations/add/restore",
-          icon: "/assets/icons/image.svg",
-        },
-        {
-          label: "Generative Fill",
-          route: "/transformations/add/fill",
-          icon: "/assets/icons/stars.svg",
-        },
-        {
-          label: "Object Remove",
-          route: "/transformations/add/remove",
-          icon: "/assets/icons/scan.svg",
-        },
-        {
-          label: "Object Recolor",
-          route: "/transformations/add/recolor",
-          icon: "/assets/icons/filter.svg",
-        },
-        {
-          label: "Background Remove",
-          route: "/transformations/add/removeBackground",
-          icon: "/assets/icons/camera.svg",
-        },
-      ],
-    },
-    {
-      label: "Videos",
-      icon: "/assets/icons/video.svg",
-      subLinks: [
-        {
-          label: "Clip Crop",
-          route: "/video/crop",
-          icon: "/assets/icons/crop.svg",
-        },
-      ],
+export const navLinksNew = [
+	{
+		label: "Home",
+		route: "/dashboard",
+		icon: "/assets/icons/home.svg",
+	},
+	{
+		label: "Images",
+		icon: "/assets/icons/image-list.svg",
+		subLinks: [
+			{
+				label: "Image Restore",
+				route: "/transformations/add/restore",
+				icon: "/assets/icons/image.svg",
+			},
+			{
+				label: "Generative Fill",
+				route: "/transformations/add/fill",
+				icon: "/assets/icons/stars.svg",
+			},
+			{
+				label: "Object Remove",
+				route: "/transformations/add/remove",
+				icon: "/assets/icons/scan.svg",
+			},
+			{
+				label: "Object Recolor",
+				route: "/transformations/add/recolor",
+				icon: "/assets/icons/filter.svg",
+			},
+			{
+				label: "Background Remove",
+				route: "/transformations/add/removeBackground",
+				icon: "/assets/icons/camera.svg",
+			},
+		],
+	},
+	{
+		label: "Videos",
+		icon: "/assets/icons/video.svg",
+		subLinks: [
+			{
+				label: "Clip Crop",
+				route: "/video/crop",
+				icon: "/assets/icons/crop.svg",
+			},
+		],
     }
-  ];
+];
 
-  export const profileLinks = [
-    {
-      label: "Profile",
-      route: "/profile",
-      icon: "/assets/icons/profile.svg",
-    },
-    {
-      label: "Buy Credits",
-      route: "/credits",
-      icon: "/assets/icons/bag.svg",
+export const profileLinks = [
+	{
+		label: "Profile",
+		route: "/profile",
+		icon: "/assets/icons/profile.svg",
+	},
+	{
+		label: "Buy Credits",
+		route: "/credits",
+		icon: "/assets/icons/bag.svg",
     }
   ]
-  
-  export const plans = [
-    {
-      _id: 1,
-      name: "Free",
-      icon: "/assets/icons/free-plan.svg",
-      price: 0,
-      credits: 20,
-      inclusions: [
-        {
-          label: "20 Free Credits",
-          isIncluded: true,
-        },
-        {
-          label: "Basic Access to Services",
-          isIncluded: true,
-        },
-        {
-          label: "Priority Customer Support",
-          isIncluded: false,
-        },
-        {
-          label: "Priority Updates",
-          isIncluded: false,
-        },
-      ],
-    },
-    {
-      _id: 2,
-      name: "Pro Package",
-      icon: "/assets/icons/free-plan.svg",
-      price: 40,
-      credits: 120,
-      inclusions: [
-        {
-          label: "120 Credits",
-          isIncluded: true,
-        },
-        {
-          label: "Full Access to Services",
-          isIncluded: true,
-        },
-        {
-          label: "Priority Customer Support",
-          isIncluded: true,
-        },
-        {
-          label: "Priority Updates",
-          isIncluded: false,
-        },
-      ],
-    },
-    {
-      _id: 3,
-      name: "Premium Package",
-      icon: "/assets/icons/free-plan.svg",
-      price: 199,
-      credits: 2000,
-      inclusions: [
-        {
-          label: "2000 Credits",
-          isIncluded: true,
-        },
-        {
-          label: "Full Access to Services",
-          isIncluded: true,
-        },
-        {
-          label: "Priority Customer Support",
-          isIncluded: true,
-        },
-        {
-          label: "Priority Updates",
-          isIncluded: true,
-        },
-      ],
-    },
-  ];
-  
-  export const transformationTypes = {
-    restore: {
-      type: "restore",
-      title: "Restore Image",
-      subTitle: "Refine images by removing noise and imperfections",
-      config: { restore: true },
-      icon: "image.svg",
+
+export const plans = [
+	{
+		_id: 1,
+		name: "Free",
+		icon: "/assets/icons/free-plan.svg",
+		price: 0,
+		credits: 20,
+		inclusions: [
+			{
+				label: "20 Free Credits",
+				isIncluded: true,
+			},
+			{
+				label: "Basic Access to Services",
+				isIncluded: true,
+			},
+			{
+				label: "Priority Customer Support",
+				isIncluded: false,
+			},
+			{
+				label: "Priority Updates",
+				isIncluded: false,
+			},
+		],
+	},
+	{
+		_id: 2,
+		name: "Pro Package",
+		icon: "/assets/icons/free-plan.svg",
+		price: 40,
+		credits: 120,
+		inclusions: [
+			{
+				label: "120 Credits",
+				isIncluded: true,
+			},
+			{
+				label: "Full Access to Services",
+				isIncluded: true,
+			},
+			{
+				label: "Priority Customer Support",
+				isIncluded: true,
+			},
+			{
+				label: "Priority Updates",
+				isIncluded: false,
+			},
+		],
+	},
+	{
+		_id: 3,
+		name: "Premium Package",
+		icon: "/assets/icons/free-plan.svg",
+		price: 199,
+		credits: 2000,
+		inclusions: [
+			{
+				label: "2000 Credits",
+				isIncluded: true,
+			},
+			{
+				label: "Full Access to Services",
+				isIncluded: true,
+			},
+			{
+				label: "Priority Customer Support",
+				isIncluded: true,
+			},
+			{
+				label: "Priority Updates",
+				isIncluded: true,
+			},
+		],
+	},
+];
+
+export const transformationTypes = {
+	restore: {
+		type: "restore",
+		title: "Restore Image",
+		subTitle: "Refine images by removing noise and imperfections",
+		config: { restore: true },
+		icon: "image.svg",
       creditFee: 5
-    },
-    removeBackground: {
-      type: "removeBackground",
-      title: "Background Remove",
-      subTitle: "Removes the background of the image using AI",
-      config: { removeBackground: true },
-      icon: "camera.svg",
-      creditFee: 10,
-    },
-    fill: {
-      type: "fill",
-      title: "Generative Fill",
-      subTitle: "Enhance an image's dimensions using AI outpainting",
-      config: { fillBackground: true },
-      icon: "stars.svg",
+	},
+	removeBackground: {
+		type: "removeBackground",
+		title: "Background Remove",
+		subTitle: "Removes the background of the image using AI",
+		config: { removeBackground: true },
+		icon: "camera.svg",
+		creditFee: 10,
+	},
+	fill: {
+		type: "fill",
+		title: "Generative Fill",
+		subTitle: "Enhance an image's dimensions using AI outpainting",
+		config: { fillBackground: true },
+		icon: "stars.svg",
       creditFee: 7
-    },
-    remove: {
-      type: "remove",
-      title: "Object Remove",
-      subTitle: "Identify and eliminate objects from images",
-      config: {
-        remove: { prompt: "", removeShadow: true, multiple: true },
-      },
-      icon: "scan.svg",
+	},
+	remove: {
+		type: "remove",
+		title: "Object Remove",
+		subTitle: "Identify and eliminate objects from images",
+		config: {
+			remove: { prompt: "", removeShadow: true, multiple: true },
+		},
+		icon: "scan.svg",
       creditFee: 6
-    },
-    recolor: {
-      type: "recolor",
-      title: "Object Recolor",
-      subTitle: "Identify and recolor objects from the image",
-      config: {
-        recolor: { prompt: "", to: "", multiple: true },
-      },
-      icon: "filter.svg",
+	},
+	recolor: {
+		type: "recolor",
+		title: "Object Recolor",
+		subTitle: "Identify and recolor objects from the image",
+		config: {
+			recolor: { prompt: "", to: "", multiple: true },
+		},
+		icon: "filter.svg",
       creditFee: 6
-    },
-  };
-  
-  export const aspectRatioOptions = {
-    "1:1": {
-      aspectRatio: "1:1",
-      label: "Square (1:1)",
-      width: 1000,
-      height: 1000,
-    },
-    "3:4": {
-      aspectRatio: "3:4",
-      label: "Standard Portrait (3:4)",
-      width: 1000,
-      height: 1334,
-    },
-    "9:16": {
-      aspectRatio: "9:16",
-      label: "Phone Portrait (9:16)",
-      width: 1000,
-      height: 1778,
-    },
-  };
-  
-  export const defaultValues = {
-    title: "",
-    aspectRatio: "",
-    color: "",
-    prompt: "",
-    publicId: "",
-  };
-  
-  export const creditFee = -1;
+	},
+};
+
+export const aspectRatioOptions = {
+	"1:1": {
+		aspectRatio: "1:1",
+		label: "Square (1:1)",
+		width: 1000,
+		height: 1000,
+	},
+	"3:4": {
+		aspectRatio: "3:4",
+		label: "Standard Portrait (3:4)",
+		width: 1000,
+		height: 1334,
+	},
+	"9:16": {
+		aspectRatio: "9:16",
+		label: "Phone Portrait (9:16)",
+		width: 1000,
+		height: 1778,
+	},
+};
+
+export const defaultValues = {
+	title: "",
+	aspectRatio: "",
+	color: "",
+	prompt: "",
+	publicId: "",
+};
+
+export const creditFee = -1;
