370b140091a06d831121197a61e0ced2aec0c513
Nestor710 <nestorlima710@gmail.com>
2025-05-19 21:31:25 -0500

    feat: card and table view

--- components/shared/Collection.tsx
+++ components/shared/Collection.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { motion, AnimatePresence } from "framer-motion";
+
 import Image from "next/image";
 import Link from "next/link";
 import { useSearchParams, useRouter } from "next/navigation";
@@ -16,11 +18,11 @@ import { IImage } from "@/lib/database/models/image.model";
 import { formUrlQuery } from "@/lib/utils";
 
 import { Button } from "../ui/button";
-
-import { Search } from "./Search";
+import { ViewToggle } from "../ui/ViewToggle";
+import { useState } from "react";
+import { Edit } from "lucide-react";
 
 export const Collection = ({
-  hasSearch = false,
   images,
   totalPages = 1,
   page,
@@ -32,6 +34,8 @@ export const Collection = ({
 }) => {
   const router = useRouter();
   const searchParams = useSearchParams();
+  const [viewMode, setViewMode] = useState<"card" | "table">("card");
+  
 
   // PAGINATION HANDLER
   const onPageChange = (action: string) => {
@@ -46,19 +50,88 @@ export const Collection = ({
     router.push(newUrl, { scroll: false });
   };
 
+  const itemVariants = {
+    hidden: { opacity: 0, y: 20 },
+    visible: { opacity: 1, y: 0 },
+    exit: { opacity: 0, y: -20 }
+  };
+
   return (
     <>
       <div className="collection-heading">
         <h2 className="h2-bold text-dark-600">Recent Edits</h2>
-        {hasSearch && <Search />}
+        {/* {hasSearch && <Search />} */}
+        <ViewToggle 
+          viewMode={viewMode}
+          onChange={setViewMode}
+        />
+      </div>
+
+      <div>
+        {
+          images.length > 0 && viewMode === "table" && (
+            <motion.div
+              variants={itemVariants}
+              initial="hidden"
+              animate="visible"
+              exit="exit"
+              transition={{ duration: 0.3, ease: "easeInOut", type: "spring" }}
+              className="w-full pb-5"
+            >
+              <div className="grid grid-cols-4">
+                <p className="col-span-1 p-20-semibold">Title</p>
+                <p className="col-span-1 p-20-semibold">Type</p>
+                <p className="col-span-1 p-20-semibold">Date</p>
+                <p className="col-span-1 p-20-semibold">Actions</p>
+              </div>
+            </motion.div>
+          )
+        }
       </div>
 
       {images.length > 0 ? (
-        <ul className="collection-list">
-          {images.map((image, i) => (
-            <Card image={image} key={image.publicId} />
-          ))}
-        </ul>
+        <motion.ul
+          className={`${viewMode === "card" ? "collection-list" : ""}`}
+          // opcional: usar `layout` si queremos animaciones de layout automático
+          layout
+          style={{ listStyle: 'none', padding: 0, margin: 0 }} // ajustar estilos si es necesario
+        >
+          <AnimatePresence>
+            {images.map((image) => {
+              if (viewMode === "card") {
+                return (
+                  <motion.div
+                    key={image.publicId}
+                    variants={itemVariants}
+                    initial="hidden"
+                    animate="visible"
+                    exit="exit"
+                    transition={{ duration: 0.3, ease: "easeInOut", type: "spring" }}
+                    style={{ marginBottom: '1rem' }}
+                    layout
+                  >
+                    <Card image={image} />
+                  </motion.div>
+                );
+              } else {
+                return (
+                  <motion.div
+                    key={image.publicId}
+                    variants={itemVariants}
+                    initial="hidden"
+                    animate="visible"
+                    exit="exit"
+                    transition={{ duration: 0.3, ease: "easeInOut", type: "spring" }}
+                    style={{ marginBottom: '1rem' }}
+                    layout
+                  >
+                    <Table image={image} />
+                  </motion.div>
+                );
+              }
+            })}
+          </AnimatePresence>
+        </motion.ul>
       ) : (
         <div className="collection-empty">
           <p className="p-20-semibold">Empty List</p>
@@ -125,5 +198,37 @@ const Card = ({ image }: { image: IImage }) => {
         </div>
       </Link>
     </li>
+  );
+};
+
+const Table = ({ image }: { image: IImage }) => {
+  const type = {
+    "restore": "Restore",
+    "removeBackground": "Remove Background",
+    "fill": "Fill",
+    "remove": "Remove Object",
+    "recolor": "Recolor"
+  };
+
+  return (
+    <>
+      <div className="grid grid-cols-4">
+        <p className="col-span-1 p-20-semibold mr-3 line-clamp-1 text-dark-600 capitalize">
+          {image.title}
+        </p>
+        <p className="col-span-1 p-20-semibold mr-3 line-clamp-1 text-dark-600">
+          {type[image.transformationType as keyof typeof type]}
+        </p>
+        <p className="col-span-1 p-20-semibold mr-3 line-clamp-1 text-dark-600">
+          {image.createdAt?.toString().split("T")[0]}
+        </p>
+        <p className="col-span-1 p-20-semibold mr-3 line-clamp-1 text-dark-600">
+          <Link href={`/transformations/${image._id}/update`}>
+            <Edit className="size-5 text-orange-500" />
+          </Link>
+        </p>
+      </div>
+      <hr className="my-1 border-orange-500" />
+    </>
   );
 };
---
+++ components/ui/ViewToggle.tsx
@@ -0,0 +1,72 @@
+"use client";
+
+import React from "react";
+import { motion } from "framer-motion";
+import { cn } from "@/lib/utils";
+import { Captions, List, Table2 } from "lucide-react";
+
+import "@/styles/button.css"
+
+type ViewMode = "card" | "table";
+
+interface ViewToggleProps {
+  viewMode: ViewMode;
+  onChange: (viewMode: ViewMode) => void;
+  className?: string;
+}
+
+export function ViewToggle({ 
+  viewMode, 
+  onChange, 
+  className 
+}: ViewToggleProps) {
+  return (
+    <div 
+      className={cn(
+        "btn relative flex h-10 w-36 rounded-full p-1 bg-orange-400", 
+        className
+      )}
+    >
+      <div className="absolute inset-0 z-0 flex rounded-full p-1.5">
+        <motion.div 
+          className="h-full w-1/2 rounded-full bg-orange-500 shadow-sm"
+          initial={false}
+          animate={{ 
+            x: viewMode === "card" ? 0 : "100%" 
+          }}
+          transition={{ 
+            type: "spring", 
+            stiffness: 500, 
+            damping: 30 
+          }}
+        />
+      </div>
+
+      <button
+        type="button"
+        className={cn(
+          "relative z-10 flex h-full w-1/2 items-center justify-center rounded-full text-sm font-medium transition-colors",
+          viewMode === "card" ? "text-white" : "text-orange-500"
+        )}
+        onClick={() => onChange("card")}
+        aria-pressed={viewMode === "card"}
+      >
+        <Captions className="h-6 w-6"/>
+        <span className="sr-only">Card View</span>
+      </button>
+
+      <button
+        type="button"
+        className={cn(
+          "relative z-10 flex h-full w-1/2 items-center justify-center rounded-full text-sm font-medium transition-colors",
+          viewMode === "table" ? "text-white" : "text-orange-500"
+        )}
+        onClick={() => onChange("table")}
+        aria-pressed={viewMode === "table"}
+      >
+        <List className="h-6 w-6"/>
+        <span className="sr-only">Table View</span>
+      </button>
+    </div>
+  );
+}
